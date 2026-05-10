72087977d18573dd25b21444f33264add4fcbec3
Fatima Asif <Fatima01asiflive@gmail.com>
2026-05-10 10:08:34 +0500

    add redux toolkit

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { signInStart,signInFailure,signInSuccess} from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+const{loading,error} = useSelector((state) => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
 
   const handlechange = (e) => {
     setFormData({
@@ -17,7 +20,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       setError(null); 
 
       
@@ -32,19 +35,16 @@ export default function SignIn() {
       const data = await res.json();
 
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
 
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       console.log("Success:", data);
       navigate("/");
 
     } catch (error) {
-      setLoading(false);
-      setError("Something went wrong! Please try again.");
+      dispatch(signInFailure("Something went wrong! Please try again."));
       console.log(error);
     }
   };
